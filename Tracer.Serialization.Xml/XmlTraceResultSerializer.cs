622e1a316b297d25251008518423ffd7b2d703bf
stepanio <stepanio@EGOR01C8>
2026-02-08 15:19:57 +0300

    Implement serialization plugins (XML, YAML) and verify tracing output

---
+++ Tracer.Serialization.Xml/XmlTraceResultSerializer.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using Tracer.Core;
+using Tracer.Serialization.Abstractions;
+
+namespace Tracer.Serialization.Xml
+{
+    public class XmlTraceResultSerializer : ITraceResultSerializer
+    {
+        public string Format => "xml";
+
+        public void Serialize(TraceResult traceResult, Stream to)
+        {
+            var root = new XElement("root",
+                traceResult.Threads.Select(t => new XElement("thread",
+                    new XAttribute("id", t.ThreadId),
+                    new XAttribute("time", $"{t.TimeMs}ms"),
+                    t.Methods.Select(MapMethod)
+                ))
+            );
+
+            root.Save(to);
+        }
+
+        private XElement MapMethod(MethodTraceResult method)
+        {
+            return new XElement("method",
+                new XAttribute("name", method.MethodName),
+                new XAttribute("time", $"{method.TimeMs}ms"),
+                new XAttribute("class", method.ClassName),
+                method.Methods.Select(MapMethod)
+            );
+        }
+    }
+}
---
+++ Tracer.Serialization.Yaml/YamlTraceResultSerializer.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Linq;
+using Tracer.Core;
+using Tracer.Serialization.Abstractions;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace Tracer.Serialization.Yaml
+{
+    public class YamlTraceResultSerializer : ITraceResultSerializer
+    {
+        public string Format => "yaml";
+
+        public void Serialize(TraceResult traceResult, Stream to)
+        {
+            var dto = new
+            {
+                threads = traceResult.Threads.Select(t => new
+                {
+                    id = t.ThreadId,
+                    time = $"{t.TimeMs}ms",
+                    methods = t.Methods.Select(m => MapMethod(m)).ToList()
+                }).ToList()
+            };
+
+            var serializer = new SerializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
+
+            using var writer = new StreamWriter(to);
+            serializer.Serialize(writer, dto);
+        }
+
+        private object MapMethod(MethodTraceResult method)
+        {
+            return new
+            {
+                name = method.MethodName,
+                @class = method.ClassName,
+                time = $"{method.TimeMs}ms",
+                methods = method.Methods.Select(m => MapMethod(m)).ToList()
+            };
+        }
+    }
+}
